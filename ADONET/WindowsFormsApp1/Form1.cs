a7da11f310464d677884261e83e880b9e68f7401
Gülşah Şahin <61495341+gulsahh1@users.noreply.github.com>
2022-02-10 19:54:18 +0300

    Add files via upload

---
+++ ADONET/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        ProductDal _productDal = new ProductDal();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadProducts(); // GetAll metodunu çağırdık.
+        }
+
+        private void LoadProducts()
+        {
+            dgwProducts.DataSource = _productDal.GetAll();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            _productDal.Add(new Product
+            {
+                Name = tbxName.Text,
+                UnitPrice = Convert.ToDecimal(tbxUnitPrice.Text),
+                StockAmount = Convert.ToInt32(tbxStockAmount.Text)
+            });
+            LoadProducts();
+            MessageBox.Show("Product Added!");
+        }
+
+        private void dgwProducts_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // seçili satırların bilgilerini update edebilmek için ilgili alanları doldurduk.
+            tbxNameUpdate.Text = dgwProducts.CurrentRow.Cells[1].Value.ToString();
+            tbxUnitPriceUpdate.Text = dgwProducts.CurrentRow.Cells[2].Value.ToString();
+            tbxStockAmountUpdate.Text = dgwProducts.CurrentRow.Cells[3].Value.ToString();
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            Product product = new Product
+            {
+                Id = Convert.ToInt32(dgwProducts.CurrentRow.Cells[0].Value.ToString()),
+                Name = tbxNameUpdate.Text,
+                UnitPrice = Convert.ToDecimal(tbxUnitPriceUpdate.Text),
+                StockAmount = Convert.ToInt32(tbxStockAmountUpdate.Text)
+            };
+            _productDal.Update(product);
+            LoadProducts();
+            MessageBox.Show("Updated!");
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(dgwProducts.CurrentRow.Cells[0].Value.ToString());
+            _productDal.Delete(id);
+            LoadProducts();
+            MessageBox.Show("Deleted!");
+        }
+    }
+}
---
+++ ADONET/WindowsFormsApp1/ProductDal.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AdoNetDemo
+{
+    class ProductDal
+    {
+        SqlConnection _connection = new SqlConnection(@"server = desktop-caso95i\sqlexpress; initial catalog=ETrade; integrated security= true");
+        public List<Product> GetAll()
+        {
+
+            ConnectionControl();
+
+            SqlCommand sqlCommand = new SqlCommand("Select * from Products", _connection); // sql'le iletişim kurduk.
+
+            SqlDataReader reader = sqlCommand.ExecuteReader(); // komutu ExecuteReader ile çalıştırdık.
+
+            List<Product> products = new List<Product>();
+
+            while (reader.Read()) // data' yı tek tek oku diyoruz. 
+            {
+                Product product = new Product // her okunan veriyi product' a aktardık. 
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    Name = reader["Name"].ToString(),
+                    UnitPrice = Convert.ToDecimal(reader["UnitPrice"]),
+                    StockAmount = Convert.ToInt32(reader["StockAmount"])
+                };
+                products.Add(product); // aktarılan her veriyi listeye ekledik.
+
+            }
+
+            reader.Close();
+            _connection.Close();
+            return products; // listeyi döndük.
+
+        }
+
+        private void ConnectionControl()
+        {
+            if (_connection.State == ConnectionState.Closed)
+            {
+                _connection.Open(); // sql baglantısını kontrol sağlayarak açtık.
+
+            }
+        }
+
+        //public DataTable GetAll2()
+        //{
+        //    
+
+        //    if (_connection.State == ConnectionState.Closed)
+        //    {
+        //        _connection.Open(); // sql baglantısını kontrol sağlayarak açtık.
+
+        //    }
+
+        //    SqlCommand sqlCommand = new SqlCommand("Select * from Products", _connection); // sql'le iletişim kurduk.
+
+        //    SqlDataReader reader = sqlCommand.ExecuteReader(); // komutu ExecuteReader ile çalıştırdık.
+
+        //    DataTable dataTable = new DataTable(); // ürünleri listeleme işlemini yaptık. en sonunda bağlantıları kapattık ve returnle sonucu döndürdük.
+        //    dataTable.Load(reader);
+        //    reader.Close();
+        //    _connection.Close();
+        //    return dataTable;
+        //}
+
+        public void Add(Product product)
+        {
+            ConnectionControl();
+            SqlCommand sqlCommand = new SqlCommand(
+                "Insert into Products values(@name, @unitPrice, @stockAmount)", _connection);
+            sqlCommand.Parameters.AddWithValue("@name", product.Name);
+            sqlCommand.Parameters.AddWithValue("@unitPrice", product.UnitPrice);
+            sqlCommand.Parameters.AddWithValue("@stockAmount", product.StockAmount);
+            sqlCommand.ExecuteNonQuery();
+
+            _connection.Close();
+
+        }
+
+        public void Update(Product product)
+        {
+            ConnectionControl();
+            SqlCommand sqlCommand = new SqlCommand(
+                "Update Products set Name = @name, UnitPrice = @unitPrice, StockAmount = @stockAmount where Id = @id", _connection);
+            sqlCommand.Parameters.AddWithValue("@name", product.Name);
+            sqlCommand.Parameters.AddWithValue("@unitPrice", product.UnitPrice);
+            sqlCommand.Parameters.AddWithValue("@stockAmount", product.StockAmount);
+            sqlCommand.Parameters.AddWithValue("@id", product.Id);
+            sqlCommand.ExecuteNonQuery();
+
+            _connection.Close();
+
+        }
+
+        public void Delete(int id)
+        {
+            ConnectionControl();
+            SqlCommand sqlCommand = new SqlCommand(
+               "Delete from Products where Id = @id", _connection);
+            sqlCommand.Parameters.AddWithValue("@id", id);
+            sqlCommand.ExecuteNonQuery();
+
+            _connection.Close();
+
+        }
+    }
+}
